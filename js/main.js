5239596ed945ef1257895f9a383c1c3e669ab780
VladyslavL <vladyslavliashenko@gmil.com>
2022-10-06 20:00:05 +0200

    added overlay to the animation

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@
     parent.style.width = parent.children[0].clientWidth + "px";
   }
 
+  if (document.querySelector('.media__overlay')) {
+    document.querySelector('.media__overlay').addEventListener('click', function (e) {
+      e.target.classList.add('hide')
+    })
+
+    document.querySelector('.media__overlay_button').addEventListener('click', function (e) {
+      document.querySelector('.media__overlay').classList.remove('hide')
+    })
+  }
+
   MicroModal.init({
     disableScroll: true,
     awaitOpenAnimation: true,
